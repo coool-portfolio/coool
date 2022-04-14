af17b7db7e76ed91202e6ba0bd227277eedc7c2b
Ian <ierickson25@gmail.com>
2022-04-13 23:51:33 -0500

    added indexCardCol and IndexCard components, but have not applied any styling or formatting

--- src/components/index/IndexCard.js
+++ src/components/index/IndexCard.js
@@ -1,7 +1,27 @@
 import React from "react";
+import IndexCardCol from "../indexCardCol/IndexCardCol";
 
-function IndexCard() {
-	return <div>IndexCard</div>;
+function IndexCard(props) {
+	return (
+		<div className='card'>
+			<IndexCardCol
+				title='Client'
+				specific='K-Swiss'
+				info='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dui tellus.'
+			/>
+			<IndexCardCol
+				title='Project'
+				specific='Hideout'
+				info='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dui tellus.'
+			/>
+			<IndexCardCol
+				title='Year'
+				specific='2021'
+				info='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dui tellus.'
+			/>
+			<img src={props.img} alt='' className='card-img-right' />
+		</div>
+	);
 }
 
 export default IndexCard;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import IndexCard from "../components/index/IndexCard";
 
 function Index() {
-	return <div>Index</div>;
+	return (
+		<div>
+			<IndexCard client='K-Swiss' project='Hideout' year='2021' />
+		</div>
+	);
 }
 
 export default Index;
