797b3406683ff4497cdab3366d90b51599aa72ed
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-04-13 19:09:02 -0500

    setup basic toolbar

---
+++ src/components/toolbar/Links.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./toolbar.css";
+
+import { NavLink } from 'react-router-dom'
+
+function Links() {
+    return (
+        <div className="links">
+            <NavLink
+                to="index"
+            >
+                Index
+            </NavLink>
+            <p> {'\u2191'} Preview {'\u2191'}</p>
+            <NavLink
+                to="Contact"
+            >
+                Contact
+            </NavLink>
+        </div>
+    )
+}
+
+export default Links;
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./toolbar.css";
+
+import { NavLink } from 'react-router-dom'
+
+import Links from "./Links"
 
 function Toolbar() {
-	return <div>COOOL</div>;
+	return (
+		<div className="toolbar">
+			<NavLink
+				to="/"
+				className="coool"
+			>
+				COOOL
+			</NavLink>
+
+			<hr />
+
+			<Links />
+		</div>
+	)
 }
 
 export default Toolbar;
