16a2d30cc9f4111675614e319f0db87060ca470d
Ian <ierickson25@gmail.com>
2022-04-27 14:52:30 -0500

    updated some card styling and arrangement so that the img fits and the columns arent cut off/by the page

--- src/components/index/IndexCard.js
+++ src/components/index/IndexCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import IndexCardCol from "../indexCardCol/IndexCardCol";
+import CardImg from "../../assets/images/K-Swiss-Img.jpeg";
+import "./indexCard.css";
 
 function IndexCard(props) {
 	return (
-		<div className='card'>
+		<div className='card columns'>
 			<IndexCardCol
 				title='Client'
 				specific='K-Swiss'
@@ -19,7 +21,10 @@ function IndexCard(props) {
 				specific='2021'
 				info='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dui tellus.'
 			/>
-			<img src={props.img} alt='' className='card-img-right' />
+			{/* <img src={props.img} alt='' className='card-img-right column' /> */}
+			<figure className='image'>
+				<img src={CardImg} alt='' className='card-img right column' />
+			</figure>
 		</div>
 	);
 }
--- src/components/indexCardCol/IndexCardCol.js
+++ src/components/indexCardCol/IndexCardCol.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./indexCardCol.css";
 
 function IndexCardCol(props) {
 	return (
-		<div classname='cardCol'>
+		<div classname='cardCol column'>
 			<h5>{props.title}</h5>
 			<h2>{props.specific}</h2>
 			<p>{props.info}</p>
