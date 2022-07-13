eb3e8e91f4e70e636db4f16d7a4367f1e8981ced
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-07-13 16:06:20 -0500

    debugging

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/',
+    '/api',
     createProxyMiddleware({
-      target: 'https://coool-website.herokuapp.com/',
+      target: '/',
       changeOrigin: true,
     })
   );
