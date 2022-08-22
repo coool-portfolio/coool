4200b68fba25233823a228858d4cd14a8adce11a
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-08-22 14:54:16 -0500

    mobile index page adjustment

--- client/src/components/indexCardCol/IndexCardCol.js
+++ client/src/components/indexCardCol/IndexCardCol.js
@@ -1,11 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./indexCardCol.css";
 
 function IndexCardCol({ project, getRandomKey }) {
 	const [showVideo, setShowVideo] = useState(false);
+	const [mobile, setMobile] = useState(false)
+
+	// watches the size of window
+	useEffect(() => {
+		window.addEventListener('resize', handleWidth)
+	})
+	// when page loads
+	useEffect(() => {
+		if (window.innerWidth <= 768) {
+			setMobile(true)
+		} else {
+			setMobile(false)
+		}
+	}, [mobile])
+	// sets true or false depending on the width
+	function handleWidth() {
+		if (window.innerWidth <= 768) {
+			setMobile(true)
+		} else {
+			setMobile(false)
+		}
+	}
+
 	// returns first half of client
 	const client = project.client.split("\n").shift()
-	
+
 	// ARRAY PARSE
 	const creditArray = project.credits.split("\n");
 	// divides array into 3 parts
@@ -28,59 +51,120 @@ function IndexCardCol({ project, getRandomKey }) {
 
 	return (
 		<div id={project._id} className='cardContainer'>
-			{/* CLIENT */}
-			<div className='is-2 cardCol column'>
-				<div className='project'>
-					<h5>Client</h5>
-					<h2 className="client">{client}</h2>
-				</div>
+			{!mobile
+				?
+				<>
+					{/* CLIENT */}
+					<div className='is-2 cardCol column'>
+						<div className='project'>
+							<h5>Client</h5>
+							<h2 className="client">{client}</h2>
+						</div>
 
-				<div className='info'>
-					{creditCol1Arr.map((credit, i) => {
-						if (credit === '') {
-							return (<br key={getRandomKey()} />)
-						} else {
-							return (<p key={getRandomKey()}>{credit}</p>)
-						}
-					})}
-				</div>
-			</div>
+						<div className='info'>
+							{creditCol1Arr.map((credit, i) => {
+								if (credit === '') {
+									return (<br key={getRandomKey()} />)
+								} else {
+									return (<p key={getRandomKey()}>{credit}</p>)
+								}
+							})}
+						</div>
+					</div>
 
-			{/* PROJECT */}
-			<div className='is-2 cardCol column'>
-				<div className='project'>
-					<h5>Project</h5>
-					<h2>{project.title}</h2>
-				</div>
+					{/* PROJECT */}
+					<div className='is-2 cardCol column'>
+						<div className='project'>
+							<h5>Project</h5>
+							<h2>{project.title}</h2>
+						</div>
 
-				<div className='info'>
-					{creditCol2Arr.map((credit, i) => {
-						if (credit === '') {
-							return (<br key={getRandomKey()}/>)
-						} else {
-							return (<p key={getRandomKey()}>{credit}</p>)
-						}
-					})}
-				</div>
-			</div>
+						<div className='info'>
+							{creditCol2Arr.map((credit, i) => {
+								if (credit === '') {
+									return (<br key={getRandomKey()} />)
+								} else {
+									return (<p key={getRandomKey()}>{credit}</p>)
+								}
+							})}
+						</div>
+					</div>
 
-			{/* YEAR */}
-			<div className='is-2 cardCol column third'>
-				<div className='project'>
-					<h5>Year</h5>
-					<h2>{project.year}</h2>
-				</div>
+					{/* YEAR */}
+					<div className='is-2 cardCol column third'>
+						<div className='project'>
+							<h5>Year</h5>
+							<h2>{project.year}</h2>
+						</div>
 
-				<div className='info'>
-					{creditCol3Arr.map((credit, i) => {
-						if (credit === '') {
-							return (<br key={getRandomKey()}/>)
-						} else {
-							return (<p key={getRandomKey()}>{credit}</p>)
-						}
-					})}
-				</div>
-			</div>
+						<div className='info'>
+							{creditCol3Arr.map((credit, i) => {
+								if (credit === '') {
+									return (<br key={getRandomKey()} />)
+								} else {
+									return (<p key={getRandomKey()}>{credit}</p>)
+								}
+							})}
+						</div>
+					</div>
+				</>
+				:
+				<>
+				{/* MOBILE */}
+					{/* TITLES */}
+					<div className='is-2 cardCol column'>
+						<div className="mobile-layout">
+							<div className='project'>
+								<h5>Client</h5>
+								<h2 className="client">{client}</h2>
+							</div>
+
+							<div className='project'>
+								<h5>Project</h5>
+								<h2>{project.title}</h2>
+							</div>
+
+							<div className='project'>
+								<h5>Year</h5>
+								<h2>{project.year}</h2>
+							</div>
+						</div>
+					</div>
+
+					{/* CREDITS */}
+					<div className='is-2 cardCol column'>
+						<div className='info'>
+							{creditCol1Arr.map((credit, i) => {
+								if (credit === '') {
+									return (<br key={getRandomKey()} />)
+								} else {
+									return (<p key={getRandomKey()}>{credit}</p>)
+								}
+							})}
+						</div>
+
+						<div className='info'>
+							{creditCol2Arr.map((credit, i) => {
+								if (credit === '') {
+									return (<br key={getRandomKey()} />)
+								} else {
+									return (<p key={getRandomKey()}>{credit}</p>)
+								}
+							})}
+						</div>
+
+						<div className='info'>
+							{creditCol3Arr.map((credit, i) => {
+								if (credit === '') {
+									return (<br key={getRandomKey()} />)
+								} else {
+									return (<p key={getRandomKey()}>{credit}</p>)
+								}
+							})}
+						</div>
+					</div>
+				</>
+			}
 
 			{/* IMG */}
 			<figure className='image column is-6'>
