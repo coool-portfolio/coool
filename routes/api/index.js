edf99c9381a4d0fc593610c140f6a52516a3254d
briannawoodruff <briannaewoodruff@gmail.com>
2022-12-12 15:14:36 -0600

    client redesign for netlify deploy

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const projectRoutes = require("./projects");
-
-// Project routes api/
-router.use("/", projectRoutes);
-// router.use("/index", projectRoutes);
-
-
-module.exports = router;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: "https://coool-website.herokuapp.com/",
+      target: "https://coool-server.herokuapp.com/",
       changeOrigin: true,
     })
   )
